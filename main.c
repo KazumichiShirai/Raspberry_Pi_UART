bad07d5e5ed71ca1be7ff7d460b4763f614843c6
KazumichiShirai <shirai@whill.jp>
2016-02-18 10:02:37 +0900

    Add registerFdToEpoll(). Modify argument in com_Mbed().

--- main.c
+++ main.c
@@ -8,6 +8,21 @@
 #include <netdb.h>
 #include <fcntl.h>
 #include <sys/epoll.h>
+#include "./com_mbed.h"
+
+#define MAX_EVENTS (10)
+
+int registerFdToEpoll(struct epoll_event *ev, int epollfd, int fd)
+{
+     /* register socket to epoll */
+     ev->events = EPOLLIN;
+     ev->data.fd = fd;
+     if (epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, ev) == -1) {
+	  fprintf(stderr, "fail epoll_ctl\n");
+	  return -1;
+     }
+     return 1;
+}
 
 void main( void )
 {
@@ -15,7 +30,7 @@ void main( void )
      struct epoll_event events[MAX_EVENTS];
      int epollfd, nfds;
      int mbed_fd; // file descriptor for UART to communicate with mbed
-     char recv_buf[128] = {};
+     char recv_buf[255] = {};
      int len;
      int i;
 
@@ -29,7 +44,6 @@ void main( void )
      /* register Fd to epoll */
      registerFdToEpoll(&ev, epollfd, mbed_fd);
 
-
      while(1){
        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
 	  if (nfds == -1) {
@@ -40,7 +54,7 @@ void main( void )
 	  for (i = 0; i < nfds; i++) {
                /** Receive Data From Mbed **/
 	      if(events[i].data.fd == mbed_fd) {
-		  len = recvDataMbed(mbed_fd, recv_buf);
+		  len = recvDataMbed(mbed_fd, recv_buf, (sizeof (recv_buf)/sizeof(recv_buf[0])));
 	      }
 
 	  }
